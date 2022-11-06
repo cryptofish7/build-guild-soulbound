85618f66481b4f28cf25dac58f306ba8d33d10a0
cryptofish7 <yocryptofish7@gmail.com>
2022-11-06 16:31:14 +0100

    CHange name

---
+++ BadBuildersClubSoulbound.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+// ERC721A Contracts v4.2.2
+// Creators: Chiru Labs
+
+pragma solidity ^0.8.4;
+
+import './ERC721ASoulbound.sol';
+import './utils/Ownable.sol';
+
+contract BadBuildersClubSoulbound is ERC721ASoulbound, Ownable {
+    
+    string private _baseTokenURI;
+
+    constructor(string memory name_, string memory symbol_) ERC721A(name_, symbol_) {}
+
+    function safeMint(address to, uint256 quantity) public onlyOwner {
+        _safeMint(to, quantity);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override (ERC721A) returns (string memory) {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length != 0 ? baseURI : '';
+    }
+}
